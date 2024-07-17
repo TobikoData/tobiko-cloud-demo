96cdf02b193420c2deed38bd1c79f9d25d49a570
Afzal Jasani <amj355@nyu.edu>
2024-07-17 14:34:06 -0700

    adding new column non breaking change

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -14,9 +14,5 @@ SELECT
   order_id,
   payment_method,
   amount / 100 AS amount, /* `amount` is currently stored in cents, so we convert it to dollars */
-  -- 'new_column' AS new_column, /* non-breaking change example  */
+  'new_column' AS new_column /* non-breaking change example  */
 FROM tcloud_demo.seed_raw_payments
-
--- how to generate unit test code without manually writing yaml by hand
--- this will generate a file in the tests/ folder: test_stg_payments.yaml
--- sqlmesh create_test tcloud_demo.stg_payments --query tcloud_demo.seed_raw_payments "select * from tcloud_demo.seed_raw_payments limit 5" 
