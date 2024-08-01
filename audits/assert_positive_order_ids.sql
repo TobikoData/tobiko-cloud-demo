3f5289ca138601b5d1ad2c7cf44266a528016442
Afzal Jasani <amj355@nyu.edu>
2024-08-01 14:04:56 -0700

    add notes to audit and add new column from afzal

--- audits/assert_positive_order_ids.sql
+++ audits/assert_positive_order_ids.sql
@@ -6,3 +6,8 @@ SELECT *
 FROM @this_model
 WHERE
   item_id < 0
+
+
+-- run "sqlmesh audit" to execute all audits
+-- this audit ensures there are no item_ids that return a negative value
+-- will be executed after each run
