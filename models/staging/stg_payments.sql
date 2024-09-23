ea94ee82f62d66de93c4fe2e332f6658198c1f7e
Afzal Jasani <amj355@nyu.edu>
2024-09-23 14:46:35 -0700

    add in tcloud

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -19,10 +19,12 @@ FROM tcloud_demo.seed_raw_payments
 
 -- how to generate unit test code without manually writing yaml by hand
 -- this will generate a file in the tests/ folder: test_stg_payments.yaml
--- sqlmesh create_test tcloud_demo.stg_payments --query tcloud_demo.seed_raw_payments "select * from tcloud_demo.seed_raw_payments limit 5" 
+-- tcloud sqlmesh create_test tcloud_demo.stg_payments --query tcloud_demo.seed_raw_payments "select * from tcloud_demo.seed_raw_payments limit 5" 
 
--- run the audits "sqlmesh audit --model tcloud_demo.stg_payments"
+-- run the audits "tcloud sqlmesh audit --model tcloud_demo.stg_payments"
 
 -- table_diff - "sqlmesh table_diff afzal_demo:prod tcloud_demo.stg_payments"
 
--- sqlmesh fetchdf "select * from tcloud_demo__afzal_demo.stg_payments"
+-- sql transpilation "sqlmesh render --dialect databricks tcloud_demo.stg_payments"
+
+-- tcloud sqlmesh fetchdf "select * from tcloud_demo__afzal_demo.stg_payments"
