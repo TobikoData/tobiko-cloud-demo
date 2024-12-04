79ac80cbbd31c3e381664ee03651c41eeaf6c557
Sung Won Chung <sungwonchung3@gmail.com>
2024-12-04 12:23:41 -0800

    better debug example

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -21,4 +21,4 @@ FROM tcloud_demo.seed_raw_payments
 -- how to generate unit test code without manually writing yaml by hand
 -- this will generate a file in the tests/ folder: test_stg_payments.yaml
 -- sqlmesh create_test tcloud_demo.stg_payments --query tcloud_demo.seed_raw_payments "select * from tcloud_demo.seed_raw_payments limit 5" 
--- Check out this debugger view in Tobiko Cloud: https://cloud.tobikodata.com/sqlmesh/tobiko/public-demo/observer/environments/prod/runs/f0af9c8fea6e4ad6b5124229bb94526e
+-- Check out this debugger view in Tobiko Cloud: https://cloud.tobikodata.com/sqlmesh/tobiko/public-demo/observer/environments/dev/runs/dac12e84a18a438d937f74245b1a6bfc?tab=explore-executions&execution_detail=overview
