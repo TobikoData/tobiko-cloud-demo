fae93f3d8c4c4d217a8f0eb754edbf5d4fe0e1fc
Sung Won Chung <sungwonchung3@gmail.com>
2025-02-25 14:24:02 -0800

    add comments

--- models/customers.sql
+++ models/customers.sql
@@ -47,13 +47,13 @@ WITH customers AS (
     orders.customer_id
 ), final AS (
   SELECT
-    customers.customer_id,
-    customers.first_name,
-    customers.last_name,
-    customer_orders.first_order,
-    customer_orders.most_recent_order,
-    customer_orders.number_of_orders,
-    customer_payments.total_amount AS customer_lifetime_value
+    customers.customer_id, /* these inline comments cascade to the data catalog automatically */ /* This is a unique identifier for a customer */
+    customers.first_name, /* Customer's first name. PII. */
+    customers.last_name, /* Customer's last name. PII. */
+    customer_orders.first_order, /* Date (UTC) of a customer's first order */
+    customer_orders.most_recent_order, /* Date (UTC) of a customer's most recent order */
+    customer_orders.number_of_orders, /* Count of the number of orders a customer has placed */
+    customer_payments.total_amount AS customer_lifetime_value /* Total value (AUD) of a customer's orders */
   FROM customers
   LEFT JOIN customer_orders
     ON customers.customer_id = customer_orders.customer_id
