527959c31c8d68ae698cbd2fd6f56070f3781a77
Afzal Jasani <amj355@nyu.edu>
2025-03-04 15:56:36 -0800

    upgrade tcloud project

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -2,6 +2,7 @@ MODEL (
   name tcloud_demo.stg_payments,
   cron '@daily',
   grain payment_id,
+  -- kind full,
   audits (UNIQUE_VALUES(columns = (
     payment_id
   )), NOT_NULL(columns = (
@@ -14,10 +15,16 @@ SELECT
   order_id,
   payment_method,
   amount / 100 AS amount,
-  -- new_column as from_afzal /* `amount` is currently stored in cents, so we convert it to dollars */
+  -- new_column as new_column_from_afzal /* `amount` is currently stored in cents, so we convert it to dollars */
   -- 'new_column' AS new_column, /* non-breaking change example  */
 FROM tcloud_demo.seed_raw_payments
 
 -- how to generate unit test code without manually writing yaml by hand
 -- this will generate a file in the tests/ folder: test_stg_payments.yaml
--- sqlmesh create_test tcloud_demo.stg_payments --query tcloud_demo.seed_raw_payments "select * from tcloud_demo.seed_raw_payments limit 5" 
+-- tcloud sqlmesh create_test tcloud_demo.stg_payments --query tcloud_demo.seed_raw_payments "select * from tcloud_demo.seed_raw_payments limit 5" 
+
+-- table_diff - "tcloud sqlmesh table_diff afzal_demo:prod tcloud_demo.stg_payments"
+
+-- sql transpilation "tcloud sqlmesh render --dialect databricks tcloud_demo.stg_payments"
+
+-- tcloud sqlmesh fetchdf "select * from tcloud_demo__afzal_demo.stg_payments"
