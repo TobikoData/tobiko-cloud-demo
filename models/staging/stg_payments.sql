2fe2740e3811cedaa0c6ca1a609558058d1b2ad5
Sung Won Chung <sungwonchung3@gmail.com>
2025-04-16 10:09:44 -0700

    fix this up

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -15,5 +15,5 @@ SELECT
   payment_method,
   'advanced_cll_column' AS advanced_cll_column,
   amount / 100 AS amount, /* `amount` is currently stored in cents, so we convert it to dollars */
-  -- 'new_column' AS new_column_demo /* non-breaking change example  */
+  -- '2' AS new_column_demos /* non-breaking change example  */
 FROM tcloud_demo.seed_raw_payments
