b52dcdd5eeac38375d6eafde953d07c82a549e74
Afzal Jasani <amj355@nyu.edu>
2025-04-18 12:37:37 -0700

    clean up branch

--- utils/xdb_diff_demo.sh
+++
@@ -1,3 +0,0 @@
-# tcloud sqlmesh table_diff 'tobiko_cloud|tcloud_demo__dev_sung.incremental_model:postgres|sqlmesh_state_demo.sqlmesh._intervals' --on id --show-sample
-tcloud sqlmesh table_diff 'postgres|sqlmesh_state_demo.sqlmesh._intervals:tobiko_cloud|tcloud_demo__dev_sung.incremental_model' --on id --show-sample
-
